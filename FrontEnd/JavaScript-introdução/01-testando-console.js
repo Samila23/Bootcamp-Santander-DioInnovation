d4b467e757a0724082fd56350a791004b76d82a3
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-06-02 15:54:22 -0300

    @Samila23 [Criando funcoes e lógica para mostrar números pares e impas, com o auxilio do console]

---
+++ FrontEnd/JavaScript-introdução/01-testando-console.js
@@ -0,0 +1,15 @@
+console.log("Hello world!")
+function returnEvenValues(array){
+    let evenNums = [];
+    for ( let i = 0; i < array.length; i++){
+        if (array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }
+        else{
+            console.log(`${array[i]} não é par!`)
+        }
+    }
+    console.log('Os numeros pares são:', evenNums);
+}
+let array = [1,2,4,5,7,8];
+returnEvenValues(array);
