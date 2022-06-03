0ca98267730b0e466591ebd5a58f91eecd2cec17
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-06-03 16:03:39 -0300

    @Samila23 [Conhecendo alguns atributos de String]

--- FrontEnd/JAVASCRIPT/Readme.md
+++ FrontEnd/JAVASCRIPT/Readme.md
@@ -2,3 +2,4 @@
 <h3>Temas abordados</h3>
 <p>Introdução: Funções, console</p>
 <p>Atribuindo Valores: variáveis (let, var, hoisting, constantes  )</p>
+<p>Tipos: Estrutura de Dados, String , Numbers, Booleans, Arrays, Objetos, Empty, undefined e null</p>
---
+++ FrontEnd/JAVASCRIPT/Tipos/string.js
@@ -0,0 +1,10 @@
+let nome = "João";
+let sobrenome = "Pedro";
+let frase = "Olá, tudo bem ?";
+
+let nome2 = new String ("Samila");
+
+console.log(`Nome: ${nome} , sobrenome: ${sobrenome}`);
+console.log(`valor da String : ${nome2} ou pode ser chamado pelas posições ${nome2[0]}`);
+console.log(frase.split(""));
+console.log(frase.split(" "));
