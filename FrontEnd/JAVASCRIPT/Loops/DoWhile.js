56f8b050aad17a8ed422b2f0916409f14eadf94d
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-06-06 15:47:36 -0300

    @Samila23 [Aplicando conceito do DoWhile]

---
+++ FrontEnd/JAVASCRIPT/Loops/DoWhile.js
@@ -0,0 +1,19 @@
+function doWhile(){
+    let num = 0; 
+
+    do {
+        console.log(num);
+        num++;
+    }while(num<=5)
+}
+
+function doWhile2(){
+    let num = 6; 
+
+    do {
+        console.log(num);
+        num++;
+    }while(num<=5)
+}
+console.log(doWhile());
+console.log(doWhile2());
