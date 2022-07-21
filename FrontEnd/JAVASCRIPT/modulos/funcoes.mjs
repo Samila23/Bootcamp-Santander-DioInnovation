a77af10847488137e47c72b30b241660c0c1848b
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-07-21 16:10:14 -0300

    @samila23 [Modulos]

---
+++ FrontEnd/JAVASCRIPT/modulos/funcoes.mjs
@@ -0,0 +1,16 @@
+function mostrarIdade (nome, idade){
+    return `A idade de ${nome} é ${idade}`;
+}
+
+function mostrarCidade (nome, cidade){
+    return `A Cidade de ${nome} é ${cidade}`;
+}
+function mostrarHobby (nome, hobby){
+    return `O hobby de ${nome} é ${hobby}`;
+}
+
+export {
+    mostrarIdade,
+    mostrarCidade,
+    mostrarHobby
+}
---
+++ FrontEnd/JAVASCRIPT/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostrarIdade , mostrarCidade , mostrarHobby } from "./funcoes.mjs";
+
+console.log(mostrarIdade('Sâmila', 25));
+console.log(mostrarCidade('Sâmila', 'Santo André'));
+console.log(mostrarHobby('Sâmila', 'Assistir'))
