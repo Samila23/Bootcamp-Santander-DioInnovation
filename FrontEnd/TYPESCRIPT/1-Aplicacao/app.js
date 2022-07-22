7b71cccae1984d9a0c789378e71c1e6742690b2a
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-07-21 21:17:02 -0300

    @samila23 [Aplicando conceitos de Typescript]

---
+++ FrontEnd/TYPESCRIPT/1-Aplicacao/app.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("Estou vinculada ao index.html");
+let button = document.getElementById('button');
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+function adicionarNumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+if (button) {
+    button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
+        if (input1 && input2) {
+            console.log(adicionarNumeros(Number(input1.value), Number(input2.value)));
+        }
+    });
+}
---
+++ FrontEnd/TYPESCRIPT/readme.md
@@ -0,0 +1,9 @@
+# Aplicando conceitos de TypeScript
+
+<p>Passo a passo a realização do projeto proposto</p>
+1 - npm init
+2 - npm install --save-dev typescript
+3 - npm install lite-server
+
+* Para rodar o arquivo TS 
+1 - tsc --init
